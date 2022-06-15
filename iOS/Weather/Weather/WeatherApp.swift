918d38ffd188db7c25388c59b1c3490e0eb69d6d
Tejas Satam <tejasmsatam24@gmail.com>
2022-06-15 15:38:47 -0500

    Adds a new Project Weather

---
+++ iOS/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tejas Satam on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
