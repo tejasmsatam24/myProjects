07e3b65fa294032928f5c4ee7c763cf354f0117d
Tejas Satam <tejasmsatam24@gmail.com>
2022-06-28 20:05:41 -0500

    Adds a program to find the sum of numbers using recursion

---
+++ Java/RecursionSumOfAllNumbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class RecursionSumOfAllNumbers {
+
+    public static void main (String[] args) {
+
+        long sumOfAllNumbers = sumOfAllNumbers(9);
+
+        System.out.println(sumOfAllNumbers);
+
+    }
+
+    public static long sumOfAllNumbers(long number) {
+
+        if(number != 0) {
+
+            return number + sumOfAllNumbers(number-1);
+
+        } else {
+
+            return number;
+
+        }
+
+    }
+
+}
