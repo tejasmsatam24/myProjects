7303816e28e55c69d3d9394e073249f2fbd8c6ac
Tejas Satam <tejasmsatam24@gmail.com>
2022-06-28 20:28:58 -0500

    Adds a program to check Palindrome using recursion

---
+++ Java/JavaPalindromeCheckProgram.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class JavaPalindromeCheckProgram {
+
+    public static void main(String[] args) {
+
+    boolean flag = palindromeCheck("sitonapanotis");
+    
+    System.out.println(flag);
+
+    flag = palindromeCheck("nine");
+
+    System.out.println(flag);
+
+    flag = palindromeCheck("amanaplanacanalpanama");
+
+    System.out.println(flag);
+
+    }
+
+    public static boolean palindromeCheck(String s) {
+
+        if(s.length() == 0 || s.length() == 1) {
+
+            return true;
+
+        }  
+
+        if(s.charAt(0) == s.charAt(s.length()-1)) {
+
+            return palindromeCheck(s.substring(1, s.length()-1));
+
+        }
+
+        return false;
+
+    }
+
+}
