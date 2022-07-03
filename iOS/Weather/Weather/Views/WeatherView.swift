40beb9c7a12d67942ead93fc90b7a470fb350c08
Tejas Satam <tejasmsatam24@gmail.com>
2022-07-03 12:23:40 -0500

    Updates WeatherView

--- iOS/Weather/Weather/Views/WeatherView.swift
+++ iOS/Weather/Weather/Views/WeatherView.swift
@@ -11,7 +11,46 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading){
+            VStack{
+                VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today,  \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack {
+                            VStack(spacing: 20){
+                                Image(systemName: "sun.max.fill")
+                                    .font(.system(size: 40))
+                                
+                                Text(weather.weather[0].main)
+                            }
+                            .frame(width: 150, alignment: .leading)
+                            
+                            Text(weather.main.feelsLike.roundDouble() + "°")
+                                .font(.system(size: 100))
+                                .fontWeight(.bold)
+                                .padding()
+                        }
+                        
+                        Spacer()
+                            .frame(height: 80)
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.534, saturation: 0.355, brightness: 0.939))
     }
 }
 
